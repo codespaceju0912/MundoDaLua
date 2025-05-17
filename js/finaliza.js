1412b1d8583cea1e7abeb8b4864c8f76715ca233
codespaceju0912 <juliaalmeidanasc8@gmail.com>
2025-05-17 14:27:24 -0300

    adicionado js

--- js/finaliza.js
+++ js/finaliza.js
@@ -2,4 +2,24 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('btVoltaPVisaoGTopper').addEventListener('click', function () {
         window.location.href = "visaoGTopper.html";
     });
+
+    document.querySelector('.submit-btn').addEventListener('click', function () {
+        const paymentMethods = document.getElementsByName('payment');
+        let selectedPayment = '';
+
+        paymentMethods.forEach(method => {
+            if (method.checked) {
+                selectedPayment = method.value;
+            }
+        });
+
+        if (selectedPayment === 'pix') {
+            window.location.href = '/paginas/pagamentoPix.html';
+        } else if (selectedPayment === 'local') {
+            window.location.href = '/paginas/pagamentoLocal.html';
+        } else {
+            alert('Por favor, selecione um método de pagamento.');
+        }
+    });
 });
+
