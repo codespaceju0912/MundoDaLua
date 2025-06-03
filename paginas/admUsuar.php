867999930f4b0a9b329d6662b440cd9c34704dae
murilo-010 <murilobatistadocarmo25081@gmail.com>
2025-06-03 20:23:04 -0300

    Renomear extensão

---
+++ paginas/admUsuar.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciar Usuários</title>
+    <link rel="stylesheet" href="/css/admUsuar.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
+    <script src="/js/base.js" defer></script>
+</head>
+<body>
+    <header>
+        <section id="bloco_mundoDaLua">
+            <h1 id="grafica">Gráfica</h1>
+            <h1 id="mundodalua">MUNDO DA LUA</h1>
+        </section>
+
+        <section id="bloco_pesquisa">
+            <input id="barradepesquisa" class="montserrat" type="text"
+                placeholder="Encontre aqui o melhor produto para você">
+            <button id="botaoPesquisar"><img src="/img/lupaBranca.png" alt="" id="imagemLupa"></button>
+        </section>
+
+        <section id="bloco_perfil">
+            <img src="/img/perfil.png" alt="" id="imgperfil">
+            <h3>Faça <a href="/paginas/login.html"><span id="avisoPerfil">LOGIN</span></a> <br>ou <br><a
+                    href="/paginas/cadastro.html"><span id="avisoPerfil">CADASTRE-SE</span></a></h3>
+        </section>
+        <section id="bloco_carrinho" onclick="goCarrinho()">
+            <img src="/img/Carrinho.png" alt="" id="imgcarrinho">
+            <h3>Carrinho</h3>
+        </section>
+    </header>
+    <nav>
+
+        <div class="btn-group dropend" id="corCategoria">
+            <button type="button" class="btn btn-link dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                Categorias
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="topper.html">Personalizados</a></li>
+                <li><a class="dropdown-item" href="quadro.html">Fotos</a></li>
+                <li><a class="dropdown-item" href="marcadores.html">Papelaria</a></li>
+            </ul>
+        </div>
+
+        <section class="palavrasNav">
+            <p>Produtos</p>
+        </section>
+        <section class="palavrasNav" onclick="goPedidos()">
+            <p>Acompanhar Pedidos</p>
+        </section>
+    </nav>
+
+    <main>
+        <section class="form-usu">
+            <h2>Cadastrar Usuário</h2>
+            <form id="form-cadUsu">
+                <label for="nomeUsu">Nome do Usuário: </label>
+                <input type="text" id="nomeUsu" name="nomeUsu" required>
+
+                <label for="emailUsu">Email do Usuário:</label>
+                <input type="email" id="emailUsu" name="emailUsu" required>
+
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" required />
+
+                <button class="cad">Cadastrar</button>
+
+            </form>
+        </section>
+
+        <section class="list-Usu">
+            <h2>Usuários Cadastrados</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody id="tabelaUsu">
+                    <!--Os usuários vão aparecer aqui-->
+                </tbody>
+            </table>
+        </section>
+    </main>
+    
+    <footer>
+
+        <div>
+            <h3>Fale conosco</h3>
+            <p>Tell: (27) 99201-0821</p>
+            <p>E-mail: omundodaluaservicos<br>digitais@gmail.com</p>
+        </div>
+        <div>
+            <h3>Redes sociais</h3>
+            <section class="redessociais">
+                <a href="https://www.instagram.com/omundodaluaservicosdigitais?igsh=ZXlzZWdlbGE1ZWhq"><img src="/img/instagram.png" alt=""></a>
+                <a href="https://www.instagram.com/omundodaluaservicosdigitais?igsh=ZXlzZWdlbGE1ZWhq"><p>@omundodalua<br>servicosdigitais</p></a>
+            </section>
+        </div>
+        <div>
+            <h3>Área do Cliente</h3>
+            <a href="/paginas/login.html">Login</a><br>
+            <a href="cadastro.html">Cadastre-se</a><br>
+            <a href="telaPedidos.html">Meus pedidos</a>
+        </div>
+        <div>
+            <h3>Sobre Nós</h3>
+            <a href="/paginas/sobreaEmpresa.html">Sobre a Empresa</a>
+        </div>
+    </footer>
+
+    <!--js para excluir e cadastrar usuários-->
+    <script src="/js/admUsuar.js"></script>
+</body>
+</html>
