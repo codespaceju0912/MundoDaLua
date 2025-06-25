eaba7fa52e3c33d42eee8421a4bc7d4df8604c01
PequenaaLua <lannayitauana.c.caldas@gmail.com>
2025-06-25 14:38:12 -0300

    .

--- paginas/verificaAdmin.php
+++ paginas/verificaAdmin.php
@@ -2,17 +2,9 @@
 
 session_start();
 
-
-//Verifica se está logado
-if(!isset($_SESSION['idUsu']) || !isset($_SESSION['tipoUsu'])) {
-    header("Location: /paginas/login.php");
-    exit();
+if (!isset($_SESSION['tipoUsu']) || $_SESSION['tipoUsu'] !== 'admin') {
+    header("Location: /paginas/index.html");
+    exit;
 }
 
-
-// Verifica se é administrador
-if($_SESSION['tipoUsu'] !== 'admin') {
-    echo "<h2>Acesso negado. Esta área é restrita a administradores.</h2>";
-    exit();
-}
 ?>
