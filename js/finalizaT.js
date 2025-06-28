23385b0b604dd4ec286380e4ce92106d38902a67
murilo-010 <murilobatistadocarmo25081@gmail.com>
2025-06-28 13:27:07 -0300

    e

--- js/finalizaT.js
+++ js/finalizaT.js
@@ -14,19 +14,12 @@ document.querySelector(".preco-btn").addEventListener("click", () => {
         }
     });
     //caso nn tenha escolido uma opçao
-    if (alertar) {
-        alert('Por favor, selecione uma opção');
-    } else {
-        //faz o calculo e tenta proteger
-        if (parseInt(myElement.value) < 0 && Number.isInteger(parseFloat(myElement.value))){ //Tratar entrada pra valores diferentes de inteiros e menores que 0
-            valor = valor * 0;
-            alert('Por favor, escolha uma quantidade de numero inteiro e maior que zero!');
-        }
-        else{
-            valor = valor * parseInt(myElement.value);
-        }
+    
+        
+        valor = valor * parseInt(myElement.value);
+        
         valorText.textContent = "R$" + valor.toFixed(2);
-    }
+    
 })
 
 
